e1aa3eeeb4a4b7866bb020e725d4bf3ba0b127ef
Benz <ruthamnong.thotsaphon@gmail.com>
2020-01-06 10:11:07 +0700

    fix filter and store data

--- tasks/filter.js
+++ tasks/filter.js
@@ -1,6 +1,12 @@
-module.exports = (data, predicate, value) => {
+module.exports = ({ data, predicate, value, state }) => {
   const res = {
-    eq: (data, value) => data === value,
+    eq: (data, value) => {
+      if (typeof data === 'string' && typeof value === 'string') {
+        return data.toLowerCase() === value.toLowerCase()
+      } else {
+        return data === value
+      }
+    },
     neq: (data, value) => data !== value,
     gt: (data, value) => data > value,
     gte: (data, value) => data >= value,
@@ -8,5 +14,8 @@ module.exports = (data, predicate, value) => {
     lte: (data, value) => data <= value,
     cont: (data, value) => data.indexOf(value) >= 0
   }[predicate](data, value)
-  return res ? 'TRUE' : 'FALSE'
+  return {
+    match: res ? 'TRUE' : 'FALSE',
+    data: state
+  }
 }
